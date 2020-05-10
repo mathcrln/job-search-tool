5e79807d9fd6c68b80b990a76de8e053e81fb839
Mathieu <ceraline.mathieu@gmail.com>
2020-05-10 19:53:22 -0400

    Adding offers JS object

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,7 +1,32 @@
-const example = 1;
-
-function testing(int) {
-  return int = int + 1;
-}
-
-testing(example);
+const jofOffers = [
+  {
+    company: "Photosnap",
+    title: "Senior Frontend Developper",
+    featured: true,
+    new: true,
+    region: "USA only",
+    requirements: ["Frontend", "Senior", "HTML", "CSS", "Javascript"],
+    timeSincePosted: 1,
+    typeOfContract: "Full Time"
+  },
+  {
+    company: "Manage",
+    title: "Fullstack Developer",
+    featured: true,
+    new: true,
+    region: "Remote",
+    requirements: ["Fullstack", "Midweight", "Python", "React"],
+    timeSincePosted: 1,
+    typeOfContract: "Part Time"
+  },
+  {
+    company: "Account",
+    title: "Junior Frontend Developer",
+    featured: false,
+    new: true,
+    region: "USA only",
+    requirements: ["Frontend", "Junior", "Sass", "React", "Javascript"],
+    timeSincePosted: 5,
+    typeOfContract: "Part Time"
+  },
+]
