ec36dc3a1a3e040556a7bda30e27ee5c911894da
Ann Herbik <45009615+ann-dev@users.noreply.github.com>
2020-05-11 00:29:34 +0200

    New Gulp formatting task

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,27 @@
-"use strict";
-
 // Load plugins
-const autoprefixer = require("autoprefixer");
-const browsersync = require("browser-sync").create();
-const cssnano = require("cssnano");
-const eslint = require("gulp-eslint");
-const gulp = require("gulp");
-const imagemin = require("gulp-imagemin");
-const newer = require("gulp-newer");
-const plumber = require("gulp-plumber");
-const postcss = require("gulp-postcss");
-const rename = require("gulp-rename");
-const sass = require("gulp-sass");
-//const webpack = require("webpack");
-//const webpackconfig = require("./webpack.config.js");
-//const webpackstream = require("webpack-stream");
+const autoprefixer = require('autoprefixer');
+const browsersync = require('browser-sync').create();
+const cssnano = require('cssnano');
+const eslint = require('gulp-eslint');
+const gulp = require('gulp');
+const imagemin = require('gulp-imagemin');
+const newer = require('gulp-newer');
+const plumber = require('gulp-plumber');
+const postcss = require('gulp-postcss');
+const prettier = require('@bdchauvette/gulp-prettier');
+const rename = require('gulp-rename');
+const sass = require('gulp-sass');
+// const webpack = require("webpack");
+// const webpackconfig = require("./webpack.config.js");
+// const webpackstream = require("webpack-stream");
 
 // BrowserSync
 function browserSync(done) {
   browsersync.init({
     server: {
-      baseDir: "./"
+      baseDir: './',
     },
-    port: 3000
+    port: 3000,
   });
   done();
 }
@@ -36,8 +35,8 @@ function browserSyncReload(done) {
 // Optimize Images
 function images() {
   return gulp
-    .src("./src/assets/images/**/*")
-    .pipe(newer("./dist/assets/images"))
+    .src('./src/assets/images/**/*')
+    .pipe(newer('./dist/assets/images'))
     .pipe(
       imagemin([
         imagemin.gifsicle({ interlaced: true }),
@@ -46,67 +45,73 @@ function images() {
           plugins: [
             {
               removeViewBox: false,
-              collapseGroups: true
-            }
-          ]
-        })
+              collapseGroups: true,
+            },
+          ],
+        }),
       ])
     )
-    .pipe(gulp.dest("./dist/assets/images"));
+    .pipe(gulp.dest('./dist/assets/images'));
 }
 
 // CSS task
 function css() {
   return gulp
-    .src("./src/styles/**/*.scss")
+    .src('./src/styles/**/*.scss')
     .pipe(plumber())
-    .pipe(sass({ outputStyle: "expanded" }))
-    .pipe(gulp.dest("./dist/styles/"))
-    .pipe(rename({ suffix: ".min" }))
+    .pipe(prettier({ singleQuote: true, tabWidth: 2 }))
+    .pipe(sass({ outputStyle: 'expanded' }))
+    .pipe(gulp.dest('./dist/styles/'))
+    .pipe(rename({ suffix: '.min' }))
     .pipe(postcss([autoprefixer(), cssnano()]))
-    .pipe(gulp.dest("./dist/styles/"))
+    .pipe(gulp.dest('./dist/styles/'))
     .pipe(browsersync.stream());
+}
+
+function format() {
+  return gulp
+    .src(['./src/styles/**/*.scss'])
+    .pipe(plumber())
+    .pipe(prettier({ singleQuote: true }))
+    .pipe(gulp.dest((file) => file.base));
 }
 
 // Lint scripts
 function scriptsLint() {
-  return gulp
-    .src(["./src/js/**/*.js"])
-    .pipe(plumber())
-    .pipe(eslint())
-    .pipe(eslint.format())
-    //.pipe(eslint.failAfterError());
+  return gulp.src(['./src/js/**/*.js']).pipe(plumber()).pipe(eslint()).pipe(eslint.format());
+  // .pipe(eslint.failAfterError());
 }
 
 // Transpile, concatenate and minify scripts
 function scripts() {
   return (
     gulp
-      .src(["./src/js/**/*.js"])
+      .src(['./src/js/**/*.js'])
       .pipe(plumber())
-      //.pipe(webpackstream(webpackconfig, webpack))
+      // .pipe(webpackstream(webpackconfig, webpack))
       // folder only, filename is specified in webpack config
-      .pipe(gulp.dest("./dist/js/"))
+      .pipe(gulp.dest('./dist/js/'))
       .pipe(browsersync.stream())
   );
 }
 
 // Watch files
 function watchFiles() {
-  gulp.watch("./src/styles/**/*.scss", css);
-  gulp.watch("./src/js/**/*.js", gulp.series(scriptsLint, scripts));
-  gulp.watch("./**/*", gulp.series(browserSyncReload));
-  gulp.watch("./assets/images/**/*", images);
+  gulp.watch('./src/styles/**/*.scss', css);
+  gulp.watch('./src/js/**/*.js', gulp.series(scriptsLint, scripts));
+  gulp.watch('./**/*', gulp.series(browserSyncReload));
+  gulp.watch('./assets/images/**/*', images);
 }
 
 // define complex tasks
 const js = gulp.series(scriptsLint, scripts);
-const build = gulp.series(gulp.parallel(css, images, js));
+const build = gulp.series(gulp.parallel(format, css, images, js));
 const watch = gulp.parallel(watchFiles, browserSync);
 
 // export tasks
 exports.images = images;
 exports.css = css;
+exports.format = format;
 exports.js = js;
 exports.build = build;
 exports.watch = watch;
